dd2a387d836b0f67362269d7194e5e9f178bfab4
DeVonJ <devonj@whitemoondreams.com>
2021-01-14 15:48:13 +0000

    Adds sitealias middleware to app settings

--- develop/develop/settings.py
+++ develop/develop/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'sitealias.middleware.CurrentSiteMiddleware', 
 ]
 
 ROOT_URLCONF = 'develop.urls'
