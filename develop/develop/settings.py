3cb125521336110aa46a7edba26b278eaa8f43e4
DeVonJ <devonj@whitemoondreams.com>
2021-01-14 15:53:28 +0000

    Adds url specific to settings

--- develop/develop/settings.py
+++ develop/develop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TESTING = sys.argv[1:2] == ['test']
--- develop/develop/urls.py
+++ develop/develop/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from sitealias.views import my_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.TESTING:
+    urlpatterns += [
+        path('test/', my_view, name="test_view"),
+    ]
